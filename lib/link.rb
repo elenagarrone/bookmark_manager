90d71c5feb39f27bcdde29b6159dd8ca23c3768b
elena <elena.15@hotmail.it>
2014-10-07 13:40:06 +0100

    added tags

--- lib/link.rb
+++ lib/link.rb
@@ -5,4 +5,16 @@ class Link
  	property :id,		Serial
  	property :title,	String
  	property :url,		String
+
+ 	has n, :tags, :through => Resource
+end
+
+class Tag
+	include DataMapper::Resource
+
+	has n, :links, :through => Resource
+
+	property :id, Serial
+	property :text, String
+
 end
--- server.rb
+++ server.rb
@@ -19,6 +19,9 @@ end
 post '/links' do
 	url = params["url"]
 	title = params["title"]
-	Link.create(:url => url, :title => title)
+	tags= params["tags"].split(" ").map do |tag|
+		Tag.first_or_create(:text => tag)
+	end
+	Link.create(:url => url, :title => title, :tags => tags)
 	redirect to '/'
 end
