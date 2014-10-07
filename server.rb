46746ba9e96e379fd4465b5ab5c913ec5b80d87c
elena <elena.15@hotmail.it>
2014-10-07 11:51:33 +0100

    created test for adding links and updated index and server file

--- server.rb
+++ server.rb
@@ -15,3 +15,10 @@ get '/' do
 	@links = Link.all
 	erb :index
 end
+
+post '/links' do
+	url = params["url"]
+	title = params["title"]
+	Link.create(:url => url, :title => title)
+	redirect to '/'
+end
