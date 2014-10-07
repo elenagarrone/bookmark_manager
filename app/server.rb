93b887c9a8409aef50d4e815d4970f116a47f280
elena <elena.15@hotmail.it>
2014-10-07 18:15:02 +0100

    clean up the code and rearranged folders

--- app/server.rb
+++ app/server.rb
@@ -1,20 +1,13 @@
 require 'sinatra'
 require 'data_mapper'
-
-env = ENV["RACK_ENV"] || "development"
-
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
-
 require './lib/link'
 require './lib/user'
 require './lib/tag'
+require_relative 'helpers/application'
+require_relative 'data_mapper_setup'
 
 enable :sessions
 set :session_secret, 'super secret'
-
-DataMapper.finalize
-
-DataMapper.auto_upgrade!
 
 
 get '/' do
@@ -48,14 +41,3 @@ post '/users' do
 	session[:user_id] = user.id
 	redirect to ('/')
 end
-
-helpers do
-	def current_user
-		@current_user ||=User.get(session[:user_id]) if session[:user_id]
-	end
-end
-
-
-
-
-
