4a24f5d9fb2dc6a101e1361898c08bf794c4f96f
elena <elena.15@hotmail.it>
2014-10-08 19:13:31 +0100

    clean up a bit

--- app/server.rb
+++ app/server.rb
@@ -1,73 +1,18 @@
 require 'sinatra'
 require 'rack-flash'
 require 'data_mapper'
-require './lib/link'
-require './lib/user'
-require './lib/tag'
+require './app/models/link'
+require './app/models/user'
+require './app/models/tag'
 require_relative 'helpers/application'
 require_relative 'data_mapper_setup'
+require_relative 'controllers/users'
+require_relative 'controllers/sessions'
+require_relative 'controllers/links'
+require_relative 'controllers/tags'
+require_relative 'controllers/application'
 
 use Rack::Flash, :sweep => true
 
 enable :sessions
 set :session_secret, 'super secret'
-
-
-get '/' do
-	@links = Link.all
-	erb :index
-end
-
-post '/links' do
-	url = params["url"]
-	title = params["title"]
-	tags= params["tags"].split(" ").map do |tag|
-		Tag.first_or_create(:text => tag)
-	end
-	Link.create(:url => url, :title => title, :tags => tags)
-	redirect to '/'
-end
-
-get '/tags/:text' do
-	tag = Tag.first(:text => params[:text])
-	@links = tag ? tag.links : []
-	erb :index
-end
-
-get '/users/new' do
-	@user = User.new
-	erb :"users/new"
-end
-
-post '/users' do
-	@user = User.create(:email => params[:email],
-					   :password => params[:password],
-					   :password_confirmation => params[:password_confirmation])
-	if @user.save
-		session[:user_id] = @user.id
-		redirect to ('/')
-	else
-		flash.now[:errors] = @user.errors.full_messages
-		erb :"/users/new"
-	end
-end
-
-get '/sessions/new' do
-	erb :"sessions/new"
-end
-
-post '/sessions' do
-	email, password = params[:email], params[:password]
-	user = User.authenticate(email, password)
-	if user
-		session[:user_id] = user.id
-		redirect to('/')
-	else
-		flash[:errors] = ["The email or password is incorrect"]
-		erb :"sessions/new"
-	end
-end
-
-delete '/sessions' do
-  "Good bye"
-end
