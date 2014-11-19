40e9e1db481ffdf29c48538b49bda33e9f093f03
elena <elena.15@hotmail.it>
2014-11-19 12:02:51 +0000

    created new route to add links and fixed tests

--- app/controllers/links.rb
+++ app/controllers/links.rb
@@ -1,9 +1,11 @@
 post '/links' do
 	url = params["url"]
 	title = params["title"]
-	tags= params["tags"].split(" ").map do |tag|
-		Tag.first_or_create(:text => tag)
-	end
+	tags = params["tags"].split(" ").map{|tag| Tag.first_or_create(:text => tag)}
 	Link.create(:url => url, :title => title, :tags => tags)
 	redirect to '/'
+end
+
+get '/links/new' do
+  erb :'links/new'
 end
