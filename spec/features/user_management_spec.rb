3b56b28e885dd44bfb2d0c4d59c89870e736c445
Elena <elena.15@hotmail.it>
2014-12-23 11:56:57 +0000

    fixed indentation in spec

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -36,41 +36,41 @@ end
 
 context "User:" do
 
-	before(:each) do
-		User.create(:email => "test@test.com",
-		:password => 'test',
-		:password_confirmation => 'test')
-	end
+  before(:each) do
+    User.create(:email => "test@test.com",
+        :password => 'test',
+        :password_confirmation => 'test')
+  end
 
-	feature "signs in" do
+  feature "signs in" do
 
-		scenario "with correct credentials" do
-			visit '/'
-			expect(page).not_to have_content("Welcome, test@test.com")
-			sign_in('test@test.com','test')
-			expect(page).to have_content("Welcome, test@test.com")
-		end
+    scenario "with correct credentials" do
+      visit '/'
+      expect(page).not_to have_content("Welcome, test@test.com")
+      sign_in('test@test.com','test')
+      expect(page).to have_content("Welcome, test@test.com")
+    end
 
-		scenario "not possible with incorrect credentials" do
-			visit '/'
-			expect(page).not_to have_content("Welcome, test@test.com")
-			sign_in('test@test.com','wrong')
-			expect(page).to have_content("Sorry, there were the following problems with the form:")
-			expect(page).to have_content("The email or password is incorrect")
-			expect(page).not_to have_content("Welcome, test@test.com")
-		end
+    scenario "not possible with incorrect credentials" do
+      visit '/'
+      expect(page).not_to have_content("Welcome, test@test.com")
+      sign_in('test@test.com','wrong')
+      expect(page).to have_content("Sorry, there were the following problems with the form:")
+      expect(page).to have_content("The email or password is incorrect")
+      expect(page).not_to have_content("Welcome, test@test.com")
+    end
 
-	end
+  end
 
-	feature 'signs out' do
+  feature 'signs out' do
 
-		scenario 'while being signed in' do
-			sign_in('test@test.com', 'test')
-			click_button "Sign out"
-			expect(page).to have_content("Thank you for visiting, come back soon!")
-			expect(page).not_to have_content("Welcome, test@test.com")
-		end
+    scenario 'while being signed in' do
+      sign_in('test@test.com', 'test')
+      click_button "Sign out"
+      expect(page).to have_content("Thank you for visiting, come back soon!")
+      expect(page).not_to have_content("Welcome, test@test.com")
+    end
 
-	end
+  end
 
 end
